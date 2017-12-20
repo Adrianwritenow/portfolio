7d852c0c8bda17e0aae76596d7fcde004a422279
Adrian Rodriguez <adrianwritenow@gmail.com>
2017-12-19 23:33:31 -0500

    added info

--- src/App.js
+++ src/App.js
@@ -1,72 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Info from "./components/info";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Switch>
 
-        <section className="one">
+          <Route exact path="/" component={Splash}/>
+          <Route exact path="/info" component={Info}/>
 
-          <div className="wrapper">
-            <div className="content">
-              <h4>Adrian Rodriguez</h4>
-              <h4>Software Developer</h4>
-            </div>
-          </div>
-        </section>
-
-        <section className="two">
-          <div className="wrapper">
-          <div className="content">
-            <h4>Experience</h4>
-          </div>
-          </div>
-        </section>
-
-        <section className="three">
-
-          <div className="wrapper">
-          <div className="content">
-            <h4>Skills</h4>
-          </div>
-            <ul>
-              <li>REACT</li>
-              <li>JSX</li>
-              <li>REDUX</li>
-              <li>JAVASCRIPT</li>
-              <li>NODE</li>
-              <li>POSTGRESS</li>
-              <li>MONGO</li>
-              <li>EXPRESS</li>
-              <li>CSS</li>
-              <li>HTML</li>
-              <li>JSON</li>
-              <li>FINAL CUT PRO X</li>
-              <li>GIT</li>
-              <li>NPM</li>
-              <li>JAVA</li>
-            </ul>
-
-          </div>
-        </section>
-
-          <section className="four">
-          <div className="flex-center">
-
-            <a href="https://github.com/Adrianwritenow"><i className="fa fa-github fa-4x icon-3d"></i></a>
-            <a href="https://www.linkedin.com/in/adrian-rodriguez-3bb968129/"><i className="fa fa-linkedin-square fa-4x icon-3d"></i></a>
-            <a href="https://www.tumblr.com/blog/adrianwritenow"><i className="fa fa-tumblr fa-4x icon-3d"></i></a>
-            <a href="https://open.spotify.com/user/1262709064"><i className="fa fa-spotify fa-4x icon-3d"></i></a>
-
-        </div>
-
-        </section>
-
-
-
-      </div>
+      </Switch>
     );
   }
 }
