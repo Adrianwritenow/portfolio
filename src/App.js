79f5eeae917fb636c512dc3cf2a0c930f6142847
Adrian Rodriguez <adrianwritenow@gmail.com>
2017-12-19 23:47:30 -0500

    added splash page

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Info from "./components/info";
+import Splash from "./components/splash";
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Switch>
-
+      <BrowserRouter>
+        <Switch>
           <Route exact path="/" component={Splash}/>
           <Route exact path="/info" component={Info}/>
-
-      </Switch>
+        </Switch>
+    </BrowserRouter>
     );
   }
 }
---
+++ src/components/splash.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Splash extends Component {
+  render() {
+    return (
+      <div className="container">
+        <span className="txt anim-text-flow">ADRIANWRITENOW</span>
+      </div>
+
+    );
+  }
+}
+
+export default Splash;
