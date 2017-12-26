95e47f3422b17800663966722dd1458c57a593be
Adrian Rodriguez <adrianwritenow@gmail.com>
2017-12-26 17:23:55 -0500

    added fixed and absolute positioning to wrappers and content divs to give scroll through effect

--- src/components/about.js
+++ src/components/about.js
@@ -17,21 +17,40 @@ class About extends Component {
             <div className="wrapper">
               <h1 className="linethru">
                 &#x0003C;ABOUT&#x0002F;&#x0003E;</h1>
+                <div className="content">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Quisque vitae dictum erat.</p>
+                </div>
             </div>
+
           </section>
 
           <section className="two">
             <div className="wrapper">
               <h1 className="linethru">
                 &#x0003C;ABOUT&#x0002F;&#x0003E;</h1>
+                <div className="content">
+                  <p>Suspendisse ac posuere dolor.
+                 Duis sit amet euismod lacus. Ut sodales mi arcu, at pulvinar sapien gravida eleifend.
+                 In semper cursus fringilla. </p>
+                </div>
             </div>
+
           </section>
 
           <section className="three">
             <div className="wrapper">
+              <div className="content">
+                <p>Duis et risus id elit rutrum volutpat.
+                Morbi mattis nibh ligula, sit amet consectetur ipsum eleifend id. Curabitur consequat tempor lectus a dignissim.
+                Duis iaculis ante justo, lobortis interdum odio condimentum ac. Integer rhoncus dapibus nisi sed porta.
+                Mauris tortor metus, consectetur quis leo a, maximus fermentum nunc. Integer maximus a felis vel tempus.
+                Praesent ullamcorper congue urna venenatis mattis.</p>
+              </div>
               <h1 className="linethru">
                 &#x0003C;ABOUT&#x0002F;&#x0003E;</h1>
             </div>
+
           </section>
 
       </div>
