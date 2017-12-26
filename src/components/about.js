2cb3ac23dab20a2a5708814afa6b42ec7992f89f
Adrian Rodriguez <adrianwritenow@gmail.com>
2017-12-26 15:23:29 -0500

    add strikethrough to compnents headers and fixed front page position and strike through

--- src/components/about.js
+++ src/components/about.js
@@ -13,39 +13,28 @@ class About extends Component {
 
         <div className="experienceWrapper">
 
-
           <section className="one">
-
             <div className="wrapper">
-              <div className="content">
               <h1 className="linethru">
                 &#x0003C;ABOUT&#x0002F;&#x0003E;</h1>
-              </div>
             </div>
           </section>
 
           <section className="two">
             <div className="wrapper">
-            <div className="content">
-            <h1 className="linethru">
-              &#x0003C;ABOUT&#x0002F;&#x0003E;</h1>
-            </div>
+              <h1 className="linethru">
+                &#x0003C;ABOUT&#x0002F;&#x0003E;</h1>
             </div>
           </section>
 
           <section className="three">
-
             <div className="wrapper">
-            <div className="content">
-            <h1 className="linethru">
-              &#x0003C;ABOUT&#x0002F;&#x0003E;</h1>
-            </div>
-
-
+              <h1 className="linethru">
+                &#x0003C;ABOUT&#x0002F;&#x0003E;</h1>
             </div>
           </section>
 
-        </div>
+      </div>
     );
   }
 }
