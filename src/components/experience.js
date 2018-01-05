b095193ce4d0f8d858a392fcf15547db51a64f8a
Adrian Rodriguez <adrianwritenow@gmail.com>
2018-01-04 22:45:33 -0500

    fixed map issue with cards

--- src/components/experience.js
+++ src/components/experience.js
@@ -20,12 +20,10 @@ class Experience extends Component {
 
       let expCards = experience.map(function(skill){
          return (
-           <div className="container">
              <div className="flip">
                <div className="front">{skill.name}</div>
                <div className="back">{skill.description}</div>
              </div>
-           </div>
          )
       });
     return (
